088f1587606359fb04ca04f99782b88ebae23052
Dorian <dorian.fouvez@student.vinci.be>
2020-10-20 17:40:47 +0200

    refactor - extract isMultipleOf3

--- FizzBuzz.php
+++ FizzBuzz.php
@@ -5,9 +5,13 @@ class FizzBuzz{
 
     public function count(int $int): string{
 
-        if($int%3 === 0) return "Fizz";
+        if($this->isMultipleOf3($int)) return "Fizz";
         if($int === 5) return "Buzz";
 
         return strval($int);
+    }
+
+    public function isMultipleOf3(int $int): bool{
+        return $int % 3 === 0;
     }
 }
